401b698512c88c63c644f57f6e0918a20c103534
bea <beatriz@stylesage.co>
2020-10-18 20:27:53 +0200

    add the function

--- pycartociudad/__init__.py
+++ pycartociudad/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """PyLadies Madrid"""
 __email__ = "madrid@pyladies.com"
 __version__ = "0.1.0"
+
+# Explicit import of the public functions
+from .route_between_two_points import route_between_two_points
