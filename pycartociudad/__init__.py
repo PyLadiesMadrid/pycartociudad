a8a0b7f34a82cf24f53d5575170ddee6e65de6b7
bea <beatriz@stylesage.co>
2020-10-24 10:58:03 +0200

    fixing conflicts to merge

--- pycartociudad/__init__.py
+++ pycartociudad/__init__.py
@@ -7,5 +7,5 @@ __version__ = "0.1.0"
 # Explicit import of the public functions
 from .geocode import geocode
 from .reverse_geocode import reverse_geocode
-from .get_location_info import get_location_info
+
 from .route_between_two_points import route_between_two_points
